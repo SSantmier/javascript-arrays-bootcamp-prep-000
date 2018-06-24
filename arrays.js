2d73d32240135c7d81da466311535869d95f4f66
Sara S <ssantmier@gmail.com>
2018-06-24 13:28:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,10 +8,10 @@ function addElementToEndOfArray(array, element) {return [...array, element]}
 
 function destructivelyAddElementToEndOfArray(array, element) {return [array.push(element)]} FIX
 
-function accessElementInArray(array, element) {return console.log(array[index])}
+function accessElementInArray(array, element) {return console.log(array[index])} FIX
 
-function destructivelyRemoveElementFromBeginningOfArray(array, element) {return array.slice( - 1)} 
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {return array.slice( - 1)} FIX
 
-function destructivelyRemoveElementFromEndOfArray(array, element) {return array.slice(0, array.lenght - 1)}
+function destructivelyRemoveElementFromEndOfArray(array, element) {return array.slice(0, array.lenght - 1)} FIX
 
-function removeElementFromBeginningOfArray(array) {return array.shift()}
+function removeElementFromBeginningOfArray(array) {return array.shift()} FIX
